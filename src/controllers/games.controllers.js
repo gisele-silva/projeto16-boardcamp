506e1499d2aec37c46cd36207536ebbad3268f26
Gisele <gisele_s@hotmail.com>
2023-05-12 15:40:41 -0300

    correção games

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -13,11 +13,13 @@ export async function newGame (req, res){
     const {name, image, stockTotal, pricePerDay} = req.body
 
     try {
-        const gameExist = await db.query(`SELECT * FROM games WHERE name = $1`, [name])
-        if (gameExist) return res.status(409).send("Jogo existente")
+        const gameExist = await db.query(`SELECT * FROM games WHERE name = $1`, [game.name])
+        if (gameExist.rowCount !== 0) return res.status(409).send("Jogo existente")
 
         await db.query(`INSERT INTO games (name, image, stockTotal, pricePerDay) 
-        VALUES ($1, $2, $3)`, [name, image, stockTotal, pricePerDay])
+        VALUES ($1, $2, $3, $4)`, [name, image, stockTotal, pricePerDay])
+
+        res.sendStatus(200)
     } catch (error) {
         return res.status(500).send(error.message) 
     }
