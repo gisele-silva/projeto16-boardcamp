363872093ebe8c53574e323e61035bb2064297c3
Gisele <gisele_s@hotmail.com>
2023-05-14 00:22:01 -0300

    correção update

--- src/schemas/clients.schema.js
+++ src/schemas/clients.schema.js
@@ -2,9 +2,9 @@ import Joi from "joi";
 
 const clientSchema = Joi.object({
     name: Joi.string().min(2).required(),
-    phone: Joi.string().min(10).max(11).required(),
-    cpf: Joi.string().length(11).required(),
+    phone: Joi.string().min(10).max(11).pattern(/^\d+$/).required(),
+    cpf: Joi.string().length(11).pattern(/^\d+$/).required(),
     birthday: Joi.date().required()
 })
 
-export default clientSchema
+export default clientSchema 
