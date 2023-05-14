bae37a366ab1492438e8422037a0b7599211fc90
Gisele <gisele_s@hotmail.com>
2023-05-13 22:46:35 -0300

    conexão

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
-import express from "express"
-import cors from "cors"
-import gamesRouter from "./routes/games.routes.js"
-import clientsRouter from "./routes/clients.routes.js"
+import express from "express";
+import cors from "cors";
+import gamesRouter from "./routes/games.routes.js";
+import clientsRouter from "./routes/clients.routes.js";
+import dotenv from "dotenv"
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.use(gamesRouter)
 app.use(clientsRouter)
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
