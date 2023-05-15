fa3f01e0907147c5c6cb7977868d0d15910abcad
Gisele <gisele_s@hotmail.com>
2023-05-15 00:10:26 -0300

    routes

--- src/routes/rents.routes.js
+++ src/routes/rents.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { allRents, deleteRents, finishRents, newRents } from "../controllers/rents.controllers.js";
 import { validateSchema } from "../middlewares/validateSchema.middleweare.js";
-import { rentsValidate } from "../middlewares/rents.middleware.js";
+import { finishValidate, rentsValidate } from "../middlewares/rents.middleware.js";
 import rentsSchema from "../schemas/rents.schema.js";
 
 const rentsRouter = Router ()
 
 rentsRouter.post("/rentals", validateSchema(rentsSchema), rentsValidate, newRents)
 rentsRouter.get("/rentals", allRents)
-rentsRouter.post("/rentals/:id/return", finishRents)
+rentsRouter.post("/rentals/:id/return", finishValidate, finishRents)
 rentsRouter.delete("/rentals/:id", deleteRents)
 
 export default rentsRouter
